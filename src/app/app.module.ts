4fafcf0b4fdd263d0b91a7a7ea9b3a25f2eb6216
7even <48033307+7even-7@users.noreply.github.com>
2023-03-15 19:41:49 -0700

    added source folder

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UiComponent } from './ui/ui.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HandtrackerComponent } from './handtracker/handtracker.component';
+import { FoodComponent } from './food/food.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UiComponent,
+    HomePageComponent,
+    HandtrackerComponent,
+    FoodComponent,
+    CheckoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
